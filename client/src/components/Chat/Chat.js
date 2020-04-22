6470077256f3c9b4f81089b7bed250be49da5b3f
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-22 19:44:39 +0100

    Fix chat.js to pass name to message component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,49 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client'; 
+import io from "socket.io-client";
+
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
-import InfoBar from '../InfoBar/InfoBar'
-import Input from '../Input/Input'
-import Messages from '../Messages/Messages'
-
 let socket;
 
-const Chat = ({location}) => {
+const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-
-  const ENDPOINT = 'localhost:5000'
+  const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
-    
-    setName(name);
+
     setRoom(room);
+    setName(name)
 
-    socket.emit('join', { name, room }, () => {
-
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
-
-    return () => {
-      socket.emit('disconnect');
-
-      socket.off();
-    }
   }, [ENDPOINT, location.search]);
-
+  
   useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages([...messages, message])
-    })
-  }, [messages])
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+  
+}, []);
 
-  //function for sending messages
   const sendMessage = (event) => {
     event.preventDefault();
 
@@ -52,16 +48,15 @@ const Chat = ({location}) => {
     }
   }
 
-  console.log(message, messages);
   return (
     <div className="outerContainer">
       <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages}/>
-        <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './Message.css';
 
+
+
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
 
